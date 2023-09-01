4c6dda082ad12945be14ad9c2b1d12573f7d368c
playerema <63883283+playerema@users.noreply.github.com>
2023-09-01 02:29:58 -0300

    Add files via upload

---
+++ ejercicio 7 - ecuaciones de 2do grado/src/ar/edu/unlu/poo/ecuacion/Ecuacion.java
@@ -0,0 +1,62 @@
+package ar.edu.unlu.poo.ecuacion;
+
+/*Se requiere implementar una clase que represente y pueda resolver una ecuación de segundo grado.
+Dada una ecuación de segundo grado de la forma y = a * x^2 + b * x + c, la fórmula para el cálculo de
+las raíces es (-b ± √((b^2) - (4 * a * c))) / (2 * a). Una vez que se crea una instancia de la clase
+no se podrán modificar sus coeficientes. Recordar que puede darse el caso donde exista una única raíz
+ y para que esto ocurra el discriminante ((b^2) - 4 * a * c) debe ser igual a 0. En este último caso,
+ en vez de retornar 2 raíces se debería retornar una sola. Existe el caso también donde necesitemos
+ calcular el valor de y con base en el valor de x, que se pasa como parámetro.
+ */
+public class Ecuacion{
+    private final Float a;
+    private final Float b;
+    private final Float c;
+
+    public Ecuacion(Float a,Float b,Float c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+
+    private boolean unaRaiz(){
+        return (Math.pow(b,2) - 4 * a * c)==0;
+    }
+
+    private boolean dosRaiz(){
+        return (Math.pow(b,2) - 4 * a * c)>0;
+    }
+
+    private boolean sinRaiz(){
+        return ((Math.pow(b,2) - 4 * a * c)<0);
+    }
+
+    public int cantidadRaices(){
+        if(this.unaRaiz()){
+            return 1;
+        } else if (this.dosRaiz()) {
+            return 2;
+        }
+        return 0;
+    }
+    public double discriminante(){
+        return (Math.pow(b,2) - 4 * a * c);
+    }
+
+    public Raiz solucion() {
+        Raiz sol = new Raiz();
+        if (this.sinRaiz()) {
+            return sol;
+        }
+
+        if(this.unaRaiz()) {
+
+            sol.setRaiz1( (-b + Math.pow( this.discriminante() , 0.5) )/ (2*a) );
+        } else if (this.dosRaiz()) {
+            sol.setRaiz1( (-b + Math.pow( this.discriminante() , 0.5) )/ (2*a) );
+            sol.setRaiz2( (-b - Math.pow( this.discriminante() , 0.5) )/ (2*a) );
+        }
+        return sol;
+    }
+}
+
---
+++ ejercicio 7 - ecuaciones de 2do grado/src/ar/edu/unlu/poo/ecuacion/Raiz.java
@@ -0,0 +1,22 @@
+package ar.edu.unlu.poo.ecuacion;
+
+public class Raiz {
+    private Double raiz1=null;
+    private Double raiz2=null;
+
+    public Double getRaiz1(){
+        return raiz1;
+    }
+
+    public Double getRaiz2(){
+        return raiz2;
+    }
+
+    public void setRaiz1(Double f){
+        raiz1 = f;
+    }
+
+    public void setRaiz2(Double f){
+        raiz2 = f;
+    }
+}
